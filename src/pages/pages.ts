da897b463e0be91d21274bf4de52d23bde609b2f
QueHayPaHacerCtg <quehaypahacerctg@gmail.com>
2017-06-23 14:47:15 -0500

    Bienvenida / Inicio de Sesión

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -2,7 +2,7 @@ import { ListMasterPage } from './list-master/list-master';
 import { SearchPage } from './search/search';
 import { WelcomePage } from './welcome/welcome';
 import { InicioDeSesionPage } from './inicio-de-sesion/inicio-de-sesion';
-import { CercaDeMPage } from './cerca-de-m/cerca-de-m';
+import { CategoriasPage } from './categorias/categorias';
 import { RegistroPage } from './registro/registro';
 import { SettingsPage } from './settings/settings';
 
@@ -10,7 +10,7 @@ import { TabsPage } from './tabs/tabs';
 
 // The page the user lands on after opening the app and without a session
 // export const FirstRunPage = CategoriasPage;
-export const FirstRunPage = RegistroPage;
+export const FirstRunPage = CategoriasPage;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
